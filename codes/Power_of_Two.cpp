6479f25278bd91f4435462916a28e4307138ed05
yutianwu <wzxingbupt@gmail.com>
2016-03-10 22:52:20 +0800

    Power of Two

--- codes/Power_of_Two.cpp
+++ codes/Power_of_Two.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n == 0) return false;
-        while (n % 2 == 0) {
-            n /= 2;
-        }
-        return n == 1 ? true : false;
+        if (n <= 0) return false;
+        return !(n & (n - 1));
     }
 };
