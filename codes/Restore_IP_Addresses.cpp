44d7f79eedf8848d8554ecf2c14e33cef95c553c
yutianwu <wzxingbupt@gmail.com>
2016-04-21 10:59:44 +0800

    restore

--- codes/Restore_IP_Addresses.cpp
+++ codes/Restore_IP_Addresses.cpp
@@ -3,27 +3,29 @@ public:
     vector<string> restoreIpAddresses(string s) {
         vector<string> result;
         vector<string> ip;
-        dfs(s, ip, result, 0);
+        dfs(s, 0, ip, result);
         return result;
     }
 
-    void dfs(string s, vector<string> &ip, vector<string> &result, size_t start) {
-        if (ip.size() == 4 && start == s.size()) {
-            result.push_back(ip[0] + '.' + ip[1] + '.' + ip[2] + '.' + ip[3]);
+    void dfs(string s, int pos, vector<string> &ip, vector<string> &result) {
+        if (pos >= s.size()) {
+            if (ip.size() == 4) {
+                string tmp = ip[0] + "." + ip[1] + "." + ip[2] + "." + ip[3];
+                result.push_back(tmp);
+            }
             return;
         }
 
-        if (s.size() - start > (4 - ip.size()) * 3) return ;
-        if (s.size() - start < (4 - ip.size())) return;
+        if (s.size() - pos < (4 - ip.size())) return;
+        if (s.size() - pos > 3 * (4 - ip.size())) return;
 
         int num = 0;
-        for (size_t i = start; i < start + 3; i++) {
+        for (int i = pos; i < 3 + pos && i < s.size(); i++) {
             num = num * 10 + (s[i] - '0');
-            if (num < 0 || num > 255) continue;
-            ip.push_back(s.substr(start, i - start + 1));
-            dfs(s, ip, result, i + 1);
+            if (num > 255) break;
+            ip.push_back(s.substr(pos, i - pos + 1));
+            dfs(s, i + 1, ip, result);
             ip.pop_back();
-
             if (num == 0) break;
         }
     }
